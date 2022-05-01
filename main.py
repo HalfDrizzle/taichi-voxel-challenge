383caa6a9309b4f43921c1af0ffc863722c4798f
HalfRain <halfdrizzle@outlook.com>
2022-05-02 00:17:43 +0800

    update to -64, 64 index

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def rgb(r, g, b):
     return vec3(r / 255.0, g / 255.0, b / 255.0)
 
 
-small_ring_radius = 16
+small_ring_radius = 28
 big_ring_radius = int(small_ring_radius * 2 / (3 ** 0.5))
 small_ring_center_offset = 3 ** 0.5 / 6 * 2 * small_ring_radius / 2
 tri_perpendicular_length = 3 ** 0.5 / 3 * small_ring_radius
@@ -31,7 +31,7 @@ def draw_radius(v, circle_center, circle_radius, precision=0.7):
 
 
 n = 64
-center_pos = [n / 2, n / 2 + 10]
+center_pos = [0, 10]
 big_ring, big_radius = get_circle((center_pos[0] + small_ring_radius, center_pos[1] + tri_perpendicular_length),
                                   (center_pos[0] - small_ring_radius, center_pos[1] + tri_perpendicular_length),
                                   (center_pos[0], center_pos[1] - tri_perpendicular_length * 2))
@@ -39,11 +39,11 @@ big_ring, big_radius = get_circle((center_pos[0] + small_ring_radius, center_pos
 
 @ti.func
 def draw_arc(v, circle_center, circle_radius, down_vector, max_degree=30, precision=0.7):
-    ob = v - circle_center
-    crossValue = ti.math.dot(ob, down_vector)
-    degree = ti.math.degrees(ti.acos(crossValue / (down_vector.norm() * ob.norm())))
-    return ti.abs(ti.math.distance(v, circle_center) - circle_radius) <= precision and degree < max_degree
-
+    # acos always equal pi/2 because f32 precision, now ti don't support f64 acos function
+    # ob = v - circle_center
+    # dotValue = ti.math.dot(ob, down_vector)
+    # degree = ti.math.degrees(ti.acos(dotValue / (down_vector.norm() * ob.norm())))
+    return ti.abs(ti.math.distance(v, circle_center) - circle_radius) <= precision and ti.abs(v[0]) <= max_degree
 
 @ti.func
 def draw_line(v, height_edge_bottom, height_edge_top, edge):
@@ -67,21 +67,21 @@ def initialize_voxels():
     arc_center2_radius = 67 / 31 * small_ring_radius
     arc_center3_radius = 97 / 31 * small_ring_radius
 
-    line_height_low = big_ring_center[1] + big_radius
+    line_height_low = big_ring_center[1] + big_radius + tri_perpendicular_length / 3
     line_height_top = arc_big_center[1] + arc_center3_radius
 
     down_vector = ti.Vector([0, -1])
 
-    for i, j in ti.ndrange(n, n):
+    for i, j in ti.ndrange((-n, n), (-n, n)):
         v = ti.Vector([i, j])
         if draw_radius(v, big_ring_center, big_radius, 0.7) \
-                or draw_radius(v, ring1_center, small_ring_radius) \
-                or draw_radius(v, ring2_center, small_ring_radius) \
-                or draw_radius(v, ring3_center, small_ring_radius) \
-                or draw_arc(v, arc1_center, arc_center1_radius, down_vector, 45, 0.5) \
-                or draw_arc(v, arc2_center, arc_center2_radius, down_vector, 60, 0.5) \
-                or draw_arc(v, arc_big_center, arc_center3_radius, down_vector, 30) \
-                or draw_line(v, line_height_top, line_height_low, 2):
+                or draw_radius(v, ring1_center, small_ring_radius, 0.8) \
+                or draw_radius(v, ring2_center, small_ring_radius, 0.8) \
+                or draw_radius(v, ring3_center, small_ring_radius, 0.8) \
+                or draw_arc(v, arc1_center, arc_center1_radius, down_vector, 30, 0.5) \
+                or draw_arc(v, arc2_center, arc_center2_radius, down_vector, 45, 0.5) \
+                or draw_arc(v, arc_big_center, arc_center3_radius, down_vector, 32) \
+                or draw_line(v, line_height_top, line_height_low, 1):
             random = ti.random()
             if random < 0.001:
                 scene.set_voxel(vec3(i, 1, j), 2, write)
